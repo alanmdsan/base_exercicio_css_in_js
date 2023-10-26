b6088549c7ab48ebc7b492b87aaa53e91187f805
alanmdsan <alanmdsan@gmail.com>
2023-10-26 16:45:35 -0300

    setting theme and global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
+import { ThemeProvider } from 'styled-components'
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+// import './global.css'
+import temaDefault from './themes/default'
+import EstiloGlobal, { Container } from './styles'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={temaDefault}>
+      <EstiloGlobal />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
-    </>
+      </Container>
+    </ThemeProvider>
   )
 }
 
